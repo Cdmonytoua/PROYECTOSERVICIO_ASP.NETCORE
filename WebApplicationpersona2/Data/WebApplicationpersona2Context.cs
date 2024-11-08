73000e7b63077a74fdd5faa1aa653b09559a5b41
cmonytoua <cdmonytoua@gmail.com>
2024-11-08 11:10:02 -0600

    new proyecto

---
+++ WebApplicationpersona2/Data/WebApplicationpersona2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationpersona2.Modelo;
+
+namespace WebApplicationpersona2.Data
+{
+    public class WebApplicationpersona2Context : DbContext
+    {
+        public WebApplicationpersona2Context (DbContextOptions<WebApplicationpersona2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationpersona2.Modelo.Persona> Persona { get; set; }
+    }
+}
---
+++ WebApplicationpersona2/Modelo/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationpersona2.Modelo
+{
+    public class Persona
+    {
+
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string fecha { get; set; }
+
+    }
+}
